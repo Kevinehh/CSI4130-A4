ae03b97fefb225b5729353ba9022b0643ae66786
JasDhindsa <jdhin060@uottawa.ca>
2025-03-30 22:30:10 -0400

    added the stars

--- asteroids/main.js
+++ asteroids/main.js
@@ -374,3 +374,30 @@ window.addEventListener("resize", () => {
     camera.updateProjectionMatrix();
     renderer.setSize(window.innerWidth, window.innerHeight);
 });
+
+
+// the stars
+const starVertices = [];
+
+for (let i = 0; i < 2000; i++) {
+    const x = THREE.MathUtils.randFloatSpread(5000);
+    const y = THREE.MathUtils.randFloatSpread(5000);
+    const z = THREE.MathUtils.randFloatSpread(5000);
+    starVertices.push(x, y, z);
+}
+const starGeometry = new THREE.BufferGeometry();
+starGeometry.setAttribute('position', new THREE.Float32BufferAttribute(starVertices, 3));
+const starMaterial = new THREE.PointsMaterial({ color: 0x888888 });
+starMaterial.size = 0.02;
+const points = new THREE.Points(starGeometry, starMaterial);
+scene.add(points);
+
+
+// sun
+const sunGeometry = new THREE.SphereGeometry(0.3, 32, 16);
+const sunMaterial = new THREE.MeshBasicMaterial({ color: 0xff9633 });
+const sun = new THREE.Mesh(sunGeometry, sunMaterial);
+
+// sun lighting
+const sunLight = new THREE.AmbientLight(0x404040, 50);
+scene.add(sunLight);
